3abc1735b1c015a3ed1b09835a32b4e6002e6617
Mr-Lies <38107541+Mr-Lies@users.noreply.github.com>
2020-06-22 18:46:48 +0200

    Add files via upload

---
+++ L3/docs/js/main.js
@@ -0,0 +1,192 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid');
+    const scoreDisplay = document.getElementById('score');
+    const width = 28;
+    let score = 0;
+    const layout = [
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+        1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 3, 0, 0, 0, 0, 0, 0, 3, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1,
+        1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1,
+        1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1,
+        1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1,
+        1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1,
+        1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1,
+        1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1,
+        1, 3, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 3, 1,
+        1, 1, 1, 1, 0, 0, 0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 0, 1, 1, 1, 4, 1, 1, 1, 2, 2, 1, 1, 1, 4, 1, 1, 1, 0, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 0, 1, 1, 1, 4, 1, 2, 2, 2, 2, 2, 2, 1, 4, 1, 1, 1, 0, 1, 1, 1, 1, 1,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 4, 1, 2, 2, 2, 2, 2, 2, 1, 4, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        1, 0, 1, 1, 1, 1, 1, 1, 1, 4, 1, 2, 2, 2, 2, 2, 2, 1, 4, 1, 1, 1, 1, 1, 1, 1, 0, 1,
+        1, 0, 0, 0, 1, 1, 0, 0, 0, 4, 1, 1, 1, 2, 2, 1, 1, 1, 4, 0, 0, 0, 1, 1, 0, 0, 0, 1,
+        1, 1, 1, 0, 1, 1, 0, 1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1, 0, 1, 1, 0, 1, 1, 1,
+        1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1,
+        1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1,
+        1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1,
+        1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1,
+        1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1,
+        1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1,
+        1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1,
+        1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1,
+        1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1,
+        1, 3, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 3, 1,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    ];
+    const squares = [];
+
+    function createBoard() {
+        for (let l = 0; l < layout.length; l++) {
+            const square = document.createElement('div');
+            grid.appendChild(square);
+            squares.push(square);
+            if (layout[l] === 0) {
+                squares[l].classList.add('dots');
+            } else if (layout[l] === 1) {
+                squares[l].classList.add('wall');
+            } else if (layout[l] === 2) {
+                squares[l].classList.add('house-wall');
+            } else if (layout[l] === 3) {
+                squares[l].classList.add('zovid');
+            }
+        }
+    }
+    createBoard();
+    let playerCurrentIndex = 30;
+    squares[playerCurrentIndex].classList.add('player');
+
+    function movePlayer(e) {
+        squares[playerCurrentIndex].classList.remove('player');
+        switch (e.keyCode) {
+            case 65:
+                if (playerCurrentIndex % width !== 0 &&
+                    !squares[playerCurrentIndex - 1].classList.contains('wall') &&
+                    !squares[playerCurrentIndex - 1].classList.contains('house-wall'))
+                    playerCurrentIndex -= 1;
+                if (squares[playerCurrentIndex - 1] === squares[363]) {
+                    playerCurrentIndex = 391;
+                }
+                break;
+            case 87:
+                if (playerCurrentIndex - width >= 0 &&
+                    !squares[playerCurrentIndex - width].classList.contains('wall') &&
+                    !squares[playerCurrentIndex - width].classList.contains('house-wall'))
+                    playerCurrentIndex -= width;
+                break;
+            case 68:
+                if (playerCurrentIndex % width < width - 1 &&
+                    !squares[playerCurrentIndex + 1].classList.contains('wall') &&
+                    !squares[playerCurrentIndex + 1].classList.contains('house-wall'))
+                    playerCurrentIndex += 1;
+                if (squares[playerCurrentIndex + 1] === squares[392]) {
+                    playerCurrentIndex = 364;
+                }
+                break;
+            case 83:
+                if (playerCurrentIndex + width < width * width &&
+                    !squares[playerCurrentIndex + width].classList.contains('wall') &&
+                    !squares[playerCurrentIndex + width].classList.contains('house-wall'))
+                    playerCurrentIndex += width;
+                break;
+            case 27:
+                setTimeout;
+                break;
+        }
+
+        function scrollToPlayer() {
+            document.getElementById('player').scrollIntoView({ behavior: 'smooth' });
+        }
+        squares[playerCurrentIndex].classList.add('player');
+        dotEaten();
+        zovidEaten();
+        checkForGameOver();
+        checkForWin();
+    }
+    document.addEventListener('keyup', movePlayer);
+
+    function dotEaten() {
+        if (squares[playerCurrentIndex].classList.contains('dots')) {
+            score++;
+            scoreDisplay.innerHTML = score;
+            squares[playerCurrentIndex].classList.remove('dots');
+            scrollToPlayer;
+        }
+    }
+
+    function zovidEaten() {
+        if (squares[playerCurrentIndex].classList.contains('zovid')) {
+            score += 10;
+            man.forEach(man => man.isScared = true);
+            setTimeout(unScareMan, 10000);
+            squares[playerCurrentIndex].classList.remove('zovid');
+        }
+    }
+
+    function unScareMan() {
+        man.forEach(man => man.isScared = false);
+    }
+    class man {
+        constructor(className, startIndex, speed) {
+            this.className = className;
+            this.startIndex = startIndex;
+            this.speed = speed;
+            this.currentIndex = startIndex;
+            this.isScared = false;
+            this.timerId = NaN;
+        }
+    }
+    man = [
+        new man('linky', 348, 100),
+        new man('stinky', 376, 300),
+        new man('winky', 351, 150),
+        new man('danny', 379, 700)
+    ];
+    man.forEach(man => {
+        squares[man.currentIndex].classList.add(man.className);
+        squares[man.currentIndex].classList.add('man');
+    });
+    man.forEach(man => moveMan(man));
+
+    function moveMan(man) {
+        const directions = [-1, +1, width, -width];
+        let direction = directions[Math.floor(Math.random() * directions.length)];
+        man.timerId = setInterval(function() {
+            if (!squares[man.currentIndex + direction].classList.contains('man') &&
+                !squares[man.currentIndex + direction].classList.contains('wall')) {
+                squares[man.currentIndex].classList.remove(man.className);
+                squares[man.currentIndex].classList.remove('man', 'scared-man');
+                man.currentIndex += direction;
+                squares[man.currentIndex].classList.add(man.className, 'man');
+            } else
+                direction = directions[Math.floor(Math.random() * directions.length)];
+            if (man.isScared) {
+                squares[man.currentIndex].classList.add('scared-man');
+            }
+            if (man.isScared && squares[man.currentIndex].classList.contains('player')) {
+                squares[man.currentIndex].classList.remove(man.className, 'man', 'scared-man');
+                man.currentIndex = man.startIndex;
+                score += 100;
+                squares[man.currentIndex].classList.add(man.className, 'man');
+            }
+            checkForGameOver();
+        }, man.speed);
+    }
+
+    function checkForGameOver() {
+        if (squares[playerCurrentIndex].classList.contains('man') &&
+            !squares[playerCurrentIndex].classList.contains('scared-man')) {
+            man.forEach(man => clearInterval(man.timerId));
+            document.removeEventListener('keyup', movePlayer);
+            setTimeout(function() { location.replace("fail.html"); }, 500);
+        }
+    }
+
+    function checkForWin() {
+        if (score >= 350) {
+            man.forEach(man => clearInterval(man.timerId));
+            document.removeEventListener('keyup', movePlayer);
+            setTimeout(function() { location.replace("pass.html"); }, 491);
+        }
+    }
+});
+//# sourceMappingURL=main.js.map
---
+++ L4/public/src/game.js
@@ -0,0 +1,147 @@
+const game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
+    preload: preload,
+    create: create,
+    update: update
+})
+
+
+let score = 0
+let scoreText
+let platforms
+let diamonds
+let cursors
+let player
+let enemy
+
+function preload() {
+
+    game.load.image('sky', './assets/background.png')
+    game.load.image('ground', './assets/platform.png')
+    game.load.image('diamond', './assets/enemy.png')
+    game.load.spritesheet('woof', './assets/woof.png', 1000, 100)
+    game.load.image('enemy', './assets/Zovid himself.png')
+}
+
+
+function create() {
+
+    game.physics.startSystem(Phaser.Physics.ARCADE)
+
+
+    game.add.sprite(0, 0, 'sky')
+
+
+    platforms = game.add.group()
+
+
+    platforms.enableBody = true
+
+
+    const ground = platforms.create(0, game.world.height - 64, 'ground')
+
+
+    ground.scale.setTo(2, 2)
+
+
+    ground.body.immovable = true
+
+
+    let ledge = platforms.create(400, 450, 'ground')
+    ledge.body.immovable = true
+
+    ledge = platforms.create(-75, 350, 'ground')
+    ledge.body.immovable = true
+
+
+    player = game.add.sprite(32, game.world.height - 1300, 'woof')
+
+
+    enemy = game.add.sprite(600, game.world.height - 420, 'enemy')
+
+
+
+    game.physics.arcade.enable(enemy)
+
+
+    game.physics.arcade.enable(player)
+
+
+    player.body.bounce.y = 0.7
+    player.body.gravity.y = 800
+    player.body.collideWorldBounds = true
+
+
+    player.animations.add('left', [0, 1], 10, true)
+    player.animations.add('right', [2, 3], 10, true)
+
+
+    diamonds = game.add.group()
+
+
+    diamonds.enableBody = true
+
+
+    for (var i = 0; i < 12; i++) {
+        const diamond = diamonds.create(i * 70, 0, 'diamond')
+
+
+
+        diamond.body.gravity.y = 1000
+        diamond.body.bounce.y = 0.5 + Math.random() * 0.7
+    }
+
+
+    scoreText = game.add.text(16, 16, '', { fontSize: '32px', fill: '#FFF' })
+
+
+    cursors = game.input.keyboard.createCursorKeys()
+}
+
+function update() {
+
+    player.body.velocity.x = 0
+
+
+    game.physics.arcade.collide(player, platforms)
+    game.physics.arcade.collide(diamonds, platforms)
+    game.physics.arcade.collide(player, enemy)
+
+
+    game.physics.arcade.overlap(player, diamonds, collectDiamond, null, this)
+
+
+    game.physics.arcade.overlap(player, enemy, collectDiamond, null, this)
+
+
+    if (cursors.left.isDown) {
+        player.body.velocity.x = -150
+        player.animations.play('left')
+    } else if (cursors.right.isDown) {
+        player.body.velocity.x = 150
+        player.animations.play('right')
+    } else {
+
+        player.animations.stop()
+    }
+
+
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.body.velocity.y = -700
+    }
+
+    if (score === 120) {
+        location.href = 'pass.html'
+    }
+}
+
+function collectDiamond(player, diamond) {
+
+    diamond.kill()
+
+
+
+
+
+    score += 10
+    scoreText.text = 'Zovid knallers: ' + score
+}
